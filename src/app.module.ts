129f926d4f06461914a60b51e848d7b5a38c3b3c
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-17 16:51:25 +0300

    feat/update module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { BeerModule } from './core/beer/beer.module';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { APP_FILTER } from '@nestjs/core';
+import { AuthModule } from './core/auth/auth.module';
+import { UsersModule } from './core/users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [BeerModule],
+  imports: [BeerModule, AuthModule, UsersModule, DatabaseModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
 export class AppModule {}
