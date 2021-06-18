005eba20c4bd87c1b9137ca46a905e075f63527e
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-18 10:17:20 +0300

    feat/add basel-links.sql

---
+++ sql/base-links.sql
@@ -0,0 +1,33 @@
+ALTER TABLE "users_info"
+
+    ADD CONSTRAINT "FK_users_info_to_users" FOREIGN KEY("user_id")
+        REFERENCES "users"("id")
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT
+;
+
+ALTER TABLE "users_roles"
+
+    ADD CONSTRAINT "FK_users_roles_to_roles" FOREIGN KEY("role_id")
+        REFERENCES "roles"("id")
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT,
+
+    ADD CONSTRAINT "FK_users_roles_to_users" FOREIGN KEY("user_id")
+        REFERENCES "users"("id")
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT
+;
+
+ALTER TABLE "endpoints_roles"
+
+    ADD CONSTRAINT "FK_endpoints_roles_to_roles" FOREIGN KEY("role_id")
+        REFERENCES "roles"("id")
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT,
+
+    ADD CONSTRAINT "FK_endpoints_roles_to_enpoints" FOREIGN KEY("endpoint_id")
+        REFERENCES "endpoints"("id")
+        ON DELETE RESTRICT
+        ON UPDATE RESTRICT
+;
