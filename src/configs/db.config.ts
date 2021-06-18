aca9095946248782c2e57d258ee5bf31b10b87a1
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-18 13:43:36 +0300

    feat/update db.config

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -6,7 +6,7 @@ const dbconfig: TypeOrmModuleOptions = {
   autoLoadEntities: true,
   synchronize: false,
   url: process.env.DATABASE_URL,
-  extra: { ssl: true, rejectUnauthorized: false },
+  extra: { ssl: { rejectUnauthorized: false } },
 };
 
 export { dbconfig };
