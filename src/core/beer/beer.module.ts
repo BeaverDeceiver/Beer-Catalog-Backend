7a8ab5512fc08bc62b1ba5d28305e4939fa865a0
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-23 10:42:56 +0300

    feat/add favorite table to module imports

--- src/core/beer/beer.module.ts
+++ src/core/beer/beer.module.ts
@@ -1,9 +1,11 @@
 import { Module, HttpModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BeerListController } from './controllers/beerList.controller';
+import { Favorite } from './entities';
 import { BeerService } from './services/beerList.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, TypeOrmModule.forFeature([Favorite])],
   controllers: [BeerListController],
   providers: [BeerService],
   exports: [],
