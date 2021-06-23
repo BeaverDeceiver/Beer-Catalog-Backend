54cb64188566a701a947bbe11379515b8786ca97
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-23 13:52:54 +0300

    feat/add usermodule dep

--- src/core/beer/beer.module.ts
+++ src/core/beer/beer.module.ts
@@ -1,6 +1,7 @@
 import { Module, HttpModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities';
+import { UsersModule } from '../users/users.module';
 import { BeerFavoritesController } from './controllers/beerFavorites.controller';
 import { BeerListController } from './controllers/beerList.controller';
 import { Favorite } from './entities';
@@ -8,7 +9,11 @@ import { BeerService } from './services/beerList.service';
 import { FavoritesService } from './services/favorites.service';
 
 @Module({
-  imports: [HttpModule, TypeOrmModule.forFeature([Favorite, User])],
+  imports: [
+    HttpModule,
+    TypeOrmModule.forFeature([Favorite, User]),
+    UsersModule,
+  ],
   controllers: [BeerListController, BeerFavoritesController],
   providers: [BeerService, FavoritesService],
   exports: [],
