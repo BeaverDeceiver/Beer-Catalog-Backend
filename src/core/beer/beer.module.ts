7c00814d138d684f78d1851cc6ee259181dcf359
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-23 11:58:14 +0300

    feat/add favorite controller & service to exports

--- src/core/beer/beer.module.ts
+++ src/core/beer/beer.module.ts
@@ -1,13 +1,15 @@
 import { Module, HttpModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BeerFavoritesController } from './controllers/beerFavorites.controller';
 import { BeerListController } from './controllers/beerList.controller';
 import { Favorite } from './entities';
 import { BeerService } from './services/beerList.service';
+import { FavoritesService } from './services/favorites.service';
 
 @Module({
   imports: [HttpModule, TypeOrmModule.forFeature([Favorite])],
-  controllers: [BeerListController],
-  providers: [BeerService],
+  controllers: [BeerListController, BeerFavoritesController],
+  providers: [BeerService, FavoritesService],
   exports: [],
 })
 export class BeerModule {}
