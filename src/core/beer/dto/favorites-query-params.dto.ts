1f9d9578d113419fe381d610398000f6873df7c3
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-23 13:53:32 +0300

    feat/remove redundant dto

--- src/core/beer/dto/favorites-query-params.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Transform } from 'class-transformer';
-import { IsNumber } from 'class-validator';
-
-export class FavoriteBodyParamsDto {
-  @IsNumber()
-  @Transform((userId) => Number(userId))
-  userId?: number;
-}
