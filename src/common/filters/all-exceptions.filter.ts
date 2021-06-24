fbae33588f04e428b84784a715bf0404e50c72e8
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-24 13:50:02 +0300

    feat/add axios request error handling

--- src/common/filters/all-exceptions.filter.ts
+++ src/common/filters/all-exceptions.filter.ts
@@ -8,14 +8,18 @@ import {
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
-  catch(exception: unknown, host: ArgumentsHost) {
+  catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
 
-    const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
+    let status: number;
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+    } else if (exception.toJSON) {
+      status = Number(exception.toJSON().message.match(/\d+/));
+    } else {
+      HttpStatus.INTERNAL_SERVER_ERROR;
+    }
 
     response.status(status).json(exception);
   }
